c81197048842afe25a39f5a8b0de07d9b4100c3d
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-07-31 21:57:55 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #python 3.7.3 
 angka = input('masukan angka pertama : ') 
 angka2 = input('Masukan angka Kedua : ')
-tambah = angka + angka2 
-print("Totalnya Adalah"),('tambah')
+kali = angka × angka2 
+print("Totalnya Adalah"), kali
