7ad040718e2f8884b801061458ce459c01e251c9
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-02 11:04:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
-#python 3.7.3 
+#python 
+def kontol():
+    os.system("clear")
+    print(f"""    __    _________________   _______ ______
+   / /   /  _/ ____/ ____/ | / / ___// ____/
+  / /    / // /   / __/ /  |/ /\__ \/ __/
+ / /____/ // /___/ /___/ /|  /___/ / /___
+/_____/___/\____/_____/_/ |_//____/_____/
+{P}[•]{B}----------------------------------------------------{P}[•]
+{P}[•] AUTHOR: Rudal-XD
+{P}[•] WHATSAPP: 62895386194665
+{B}[•] FACEBOOK: Gaming Bubrah""")
 masuk = input('angka1 : ') 
 masuk = input('angka2 : ')
-tambah = angka1 + angka2
+tambah = masuk + masuk
 print("Totalnya Adalah"), tambah
